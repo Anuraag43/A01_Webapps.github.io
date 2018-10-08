876241d3bb72f7055b82cfa345c74e12d05099e8
Anuraag Naidu Sanakkayala <anuraagsan7@gmail.com>
2018-10-07 21:31:27 -0500

    A02 changes done
    Updated bmi page.
    Included JavaScript to calculate bmi.

--- bmi.js
+++ bmi.js
@@ -7,8 +7,8 @@ function calculateBMI(){
     let wt1 = document.getElementById("weight").value;
     let wtUnits = document.getElementById("weightunits").value;
 
-let ht = parseInt(ht1);
-let wt = parseInt(wt1);
+let ht = parseFloat(ht1);
+let wt = parseFloat(wt1);
 
     
     if(ht1 === "")
@@ -21,34 +21,34 @@ let wt = parseInt(wt1);
     }   
 
 // conveting to metric units
-if (htUnits == "inches") height /= 39.3700787;
-if (wtUnits == "lb") weight /= 2.20462;
+if (htUnits == "inches")
+{ht = ht /39.3700787;}
+
+if (wtUnits == "lbs")
+{wt = wt / 2.20462;}
 
 //calculation
-height = ht * ht;
-bmi = wt/height
-
-final_bmi = Math.floor(bmi)
-dif = bmi - final_bmi;
-dif = dif * 10;
+fh = ht * ht;
+bmi = (wt/(fh));
+bmi = bmi.toFixed(2);
 
 
-
- 
 
 //display result
 document.getElementById("output").innerHTML =bmi;
 
-let output = Math.round(BMI * 100)/100
-if(output < 18.5)
+if(bmi < 18.5){
     document.getElementById("comment").innerHTML = "Underweight";
-else if (output >= 18.5 && output <= 25)
+}
+else if (bmi >= 18.5 && bmi < 25.00){
     document.getElementById("comment").innerHTML = "Normal";
-else if(output >= 25 && output <= 25)
-    documet.getElementById("comment").innerHTML = "Obese";
-else if(output > 30)
-    document.getElementById("comment").innerHTML = "Overweight"
-
+}
+else if(bmi >= 25.01 && bmi < 30){
+    document.getElementById("comment").innerHTML = "Overweight";
+}
+else if(bmi >= 30){
+    document.getElementById("comment").innerHTML = "Obese";
+}
 
     
 }
